037e4f7e3c7951ce5a8addfcd3b93a19c13e7538
odmi-n <odamin619@gmail.com>
2025-09-26 18:40:05 +0900

    テンプレート

--- polls/main/views.py
+++ polls/main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):  # ビュー関数を定義
-    return render(request, "main/index.html")
+def index(request):
+    question_list = [
+        "プログラミングは好きですか？",
+        "数学は好きですか？",
+        "国語は好きですか？",
+    ]
+    context = {
+        "question_list": question_list,
+        "is_polled": True,
+        "polled_msg": "投票ありがとうございました。",
+        "not_polled_msg": "投票をお願いします。",
+    }
+    return render(request, "main/index.html", context)
